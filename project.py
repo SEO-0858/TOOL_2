fa17dc2ea2458237f6140059df0e92d6222c9b35
SEO-0858 <sspon1270@gmail.com>
2026-06-07 00:41:54 +0900

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- project.py
+++ project.py
@@ -285,7 +285,7 @@ if qr_scanned_serial:
             flow_error_msg = "⚠️ [데이터 누락] 툴 폐기 처리를 하려면 [교체 작업자 이름]을 반드시 입력해야 합니다!"
 
         if flow_error_msg:
-            st.error(flow_error_msg)
+            add_error(flow_error_msg)
 
         if u_submit_form_btn:
             if flow_error_msg:
@@ -893,7 +893,7 @@ else:
                                     flow_error_msg = "⚠️ [데이터 누락 방지] '사용전' 대기 상태를 제외한 가동/보관 데이터 등록 시에는 [교체 작업자 이름] 및 [기계 가공 호기(0호기 불가)]를 반드시 완벽하게 기입해야 합니다!"
 
                                 if flow_error_msg:
-                                    st.error(flow_error_msg)
+                                    add_error(flow_error_msg)
 
                                 # [최종 수정] 사용전에서 넘어온 '폐기'는 이 차단막을 아예 건드리지 않음
                                 if ed_status in ["재사용", "재사용대기"]:
