8c7107c323564e8fe682da0993b29fa3d3c306d2
SEO-0858 <sspon1270@gmail.com>
2026-06-23 18:35:18 +0900

    Update mong.py

--- mong.py
+++ mong.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 def render_search_menu():
     st.sidebar.write("### 검색 옵션")
-    if st.sidebar.button("⚙️ 스펙 중심 검색"):
+    if st.sidebar.button("⚙️ 재고 검색"):
         st.session_state['search_mode'] = 'spec'
-    if st.sidebar.button("🏭 제조사 중심 검색"):
+    if st.sidebar.button("🏭 데이터베이스 BACK UP"):
+        st.session_state['search_mode'] = 'make'
+    if st.sidebar.button("🏭 데이터베이스  RECOVER"):
         st.session_state['search_mode'] = 'make'
