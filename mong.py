fa5392bfef82fb5e66f9e6f074e6499c1094184e
SEO-0858 <sspon1270@gmail.com>
2026-06-23 18:46:17 +0900

    Update mong.py

--- mong.py
+++ mong.py
@@ -4,9 +4,10 @@ def render_search_menu():
     st.sidebar.write("### 검색 옵션")
     # 1. 사이드바 라디오 버튼으로 모드 선택
     mode = st.sidebar.radio(
-        "작업을 선택하세요",
-        ["재고 검색", "데이터베이스 BACK UP", "데이터베이스 RECOVER"]
-    )
+    "작업을 선택하세요",
+    ["재고 검색", "데이터베이스 BACK UP", "데이터베이스 RECOVER"],
+    key="tool_management_radio" # 이 key가 중복을 막아줍니다
+)
 
     # 2. 선택된 모드에 따라 오른쪽 메인 화면을 다르게 그리기
     if mode == "재고 검색":
